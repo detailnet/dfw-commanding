1c91a0ae4084ce57f2ef8c1ef872cf5221659600
Ivan Wolf <iwolf@detailag.ch>
2016-03-18 18:41:03 +0100

    Replaced call of deprecated method

--- src/Detail/Commanding/Service/CommandDispatcherAwareTrait.php
+++ src/Detail/Commanding/Service/CommandDispatcherAwareTrait.php
@@ -43,7 +43,7 @@ trait CommandDispatcherAwareTrait
             );
         }
 
-        return $commandDispatcher->handle($command);
+        return $commandDispatcher->dispatch($command);
     }
 
     /**
